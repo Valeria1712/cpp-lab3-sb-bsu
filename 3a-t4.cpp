4d8e4c3e3f4e0613572710f01a1457c66a118305
Valeria1712 <valeria1712006@gmail.com>
2024-10-26 12:25:50 +0300

    Create 3a-t4.cpp

---
+++ 3a-t4.cpp
@@ -0,0 +1,14 @@
+#include&lt;iostream&gt;
+using namespace std;
+int main()
+{
+	int k, i = 1, sum_k = 0, number;
+	cout& lt; &lt; &quot; Enter the amount of integers to sum = &quot;; cin& gt; &gt; k;
+	do {
+		cout& lt; &lt; &quot; Enter integer nr.& quot; &lt; &lt; i& lt; &lt; &quot; : &quot;;
+		cin& gt; &gt; number;
+		sum_k += number; i++;
+	} while (i& lt; = k);
+	cout& lt; &lt; &quot; The total sum of& quot; &lt; &lt; k& lt; &lt; &quot; integers is : &quot; &lt; &lt; sum_k;
+	return 0;
+}
