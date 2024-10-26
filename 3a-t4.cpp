7cf33bc6a73c8627042476e2a5dc9b1374b2ba75
Valeria1712 <valeria1712006@gmail.com>
2024-10-26 12:51:07 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,21 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
     int k, i = 1, sum_k = 0, number;
-    cout << "Enter the amount of integers to sum = "; /*allows the user to understand what to enter*/
-    cin >> k; /*data entry*/
-    if (k == 0) /*condition*/
-    {
-        cout << "The total sum: 0" << endl; /*previously, if we entered zero, we were asked for one value, now the final amount is immediately displayed to us*/
+    cout << "Enter the amount of integers to sum = ";
+    cin >> k;
+
+    while (cin.fail()) { /* this command will be executed until an incorrect value is entered in cin. for example, in the value k we will enter the letter.*/
+        cin.clear(); /* this command clears the error and allows us to continue working further*/
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); /* ignores all characters up to a new line*/
+        cout << "Enter an integer: "; /* and repeat the input of the value (with the hope that we will finally read and enter the correct one)*/
+        cin >> k;
     }
-    else { /*if k is not equal to zero, perform the following:*/
-        do {
-            cout << "Enter integer nr. " << i << ": ";
+
+    do {
+        cout << "Enter integer  " << i << ": ";
+        cin >> number;
+
+        while (cin.fail()) {
+            cin.clear(); /* I repeated this command here so that the values pass the same check. Because then the input value does not work correctly*/
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << " Enter an integer: ";
             cin >> number;
-            sum_k += number;
-            i++/*for each step in the loop, it adds a value*/
-        } while (i <= k); /*when the number of entered values is equal to k, then the program will enter the next line*/
-        cout << "The total sum of " << k << " integers is: " << sum_k << endl;
-    }
+        }
+
+        sum_k += number;
+        i++;
+    } while (i <= k);
+
+    cout << "The total sum of " << k << " integers is: " << sum_k;
     return 0;
 }
+
